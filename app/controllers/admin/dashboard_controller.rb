eb62baa863c9a477f1efb2788eb8a0fd8182414c
Karen <to.karenngo@gmail.com>
2022-02-04 01:05:16 -0500

    Add Admin dashboard to include counts and like to admin/products with Edit Inventory button

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASS']
   def show
+    @num_products = Product.count
+    @num_category = Category.count
   end
 end
