2e45c64ade219cf5f70183c1eef8ea9de15781a4
Karen <to.karenngo@gmail.com>
2022-02-04 21:15:11 -0500

    Add error validation to new category form, remove p from category instance from controller and add validate line to model

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,9 +9,9 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @pcategory = Category.new(category_params)
+    @category = Category.new(category_params)
 
-    if @pcategory.save
+    if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
